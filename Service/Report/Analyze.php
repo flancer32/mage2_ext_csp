8621ad0b2f620aa64322aa4c188f7b54294d744d
Alex Gusev <alex@flancer64.com>
2020-05-14 21:25:54 +0300

    OWN-153 Two report collectors for admin & front

--- Service/Report/Analyze.php
+++ Service/Report/Analyze.php
@@ -5,6 +5,7 @@
  */
 
 namespace Flancer32\Csp\Service\Report;
+
 use Flancer32\Csp\Api\Repo\Data\Report as EReport;
 use Flancer32\Csp\Api\Repo\Data\Rule as ERule;
 
@@ -32,24 +33,6 @@ class Analyze
         $this->daoReport = $daoReport;
         $this->daoRule = $daoRule;
         $this->daoTypePolicy = $daoTypePolicy;
-    }
-
-    /**
-     * @param \Flancer32\Csp\Service\Report\Analyze\Request $request
-     * @return \Flancer32\Csp\Service\Report\Analyze\Response
-     */
-    public function execute($request)
-    {
-        assert($request instanceof \Flancer32\Csp\Service\Report\Analyze\Request);
-        $result = new \Flancer32\Csp\Service\Report\Analyze\Response();
-        /** @var EReport[] $reports */
-        $reports = $this->daoReport->getSet();
-        $types = $this->getTypesMap();
-        $rulesToAdd = $this->convertReportsToRules($reports, $types);
-        $rulesExist = $this->getRulesExist();
-        $rulesNew = $this->compareRules($rulesToAdd, $rulesExist);
-        $this->addRules($rulesNew);
-        return $result;
     }
 
     /**
@@ -87,38 +70,16 @@ class Analyze
     }
 
     /**
-     * Build map for policy types IDs.
+     * Compose key for rules in array.
      *
-     * @return array [type_key => type_id]
+     * @param boolean $isAdmin
+     * @param int $typeId
+     * @param string $source
+     * @return string
      */
-    private function getTypesMap()
+    private function composeKey($isAdmin, $typeId, $source)
     {
-        $result = [];
-        $all = $this->daoTypePolicy->getSet();
-        foreach ($all as $one) {
-            $key = $one->getKey();
-            $id = $one->getId();
-            $result[$key] = $id;
-        }
-        return $result;
-    }
-
-    /**
-     * Get existing rules from DB.
-     *
-     * @return ERule[]
-     */
-    private function getRulesExist()
-    {
-        $result = [];
-        $all = $this->daoRule->getSet();
-        foreach ($all as $one) {
-            $typeId = $one->getTypeId();
-            $source = $one->getSource();
-            $key = "$typeId:$source";
-            $result[$key] = $one;
-        }
-        return $result;
+        return "$isAdmin:$typeId:$source";
     }
 
     /**
@@ -137,18 +98,74 @@ class Analyze
             $directive = $reportObj->{'violated-directive'};
             if (isset($types[$directive])) {
                 $typeId = $types[$directive];
+                $isAdmin = $one->getAdminArea();
                 $uriFull = $reportObj->{'blocked-uri'};
                 $parts = parse_url($uriFull);
                 $host = isset($parts['host']) ? trim(strtolower($parts['host'])) : null;
                 if ($host) {
                     $rule = new ERule();
+                    $rule->setAdminArea($isAdmin);
                     $rule->setTypeId($typeId);
                     $rule->setSource($host);
                     // collect the same rules
-                    $key = "$typeId:$host";
+                    $key = $this->composeKey($isAdmin, $typeId, $host);
                     $result[$key] = $rule;
                 }
             }
+        }
+        return $result;
+    }
+
+    /**
+     * @param \Flancer32\Csp\Service\Report\Analyze\Request $request
+     * @return \Flancer32\Csp\Service\Report\Analyze\Response
+     */
+    public function execute($request)
+    {
+        assert($request instanceof \Flancer32\Csp\Service\Report\Analyze\Request);
+        $result = new \Flancer32\Csp\Service\Report\Analyze\Response();
+        /** @var EReport[] $reports */
+        $reports = $this->daoReport->getSet();
+        $types = $this->getTypesMap();
+        $rulesToAdd = $this->convertReportsToRules($reports, $types);
+        $rulesExist = $this->getRulesExist();
+        $rulesNew = $this->compareRules($rulesToAdd, $rulesExist);
+        $this->addRules($rulesNew);
+        return $result;
+    }
+
+    /**
+     * Get existing rules from DB.
+     *
+     * @return ERule[]
+     */
+    private function getRulesExist()
+    {
+        $result = [];
+        $all = $this->daoRule->getSet();
+        foreach ($all as $one) {
+            $typeId = $one->getTypeId();
+            $isAdmin = $one->getAdminArea();
+            $source = $one->getSource();
+            $key = $this->composeKey($isAdmin, $typeId, $source);
+            $result[$key] = $one;
+        }
+        return $result;
+    }
+
+    /**
+     * Build map for policy types IDs.
+     *
+     * @return array [type_key => type_id]
+     */
+    private function getTypesMap()
+    {
+        $result = [];
+        $all = $this->daoTypePolicy->getSet();
+        foreach ($all as $one) {
+            $key = $one->getKey();
+            $id = $one->getId();
+            $result[$key] = $id;
         }
         return $result;
     }
